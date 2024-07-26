c00e88bf8971e0e84d33bfc230353901baa316d9
sikatwc3 <sikatwc3@gmail.com>
2024-07-26 02:47:41 +0000

    another perbaikan

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-      <div className="bg-blue-600 h-[250px] w-full flex justify-center items-center mt-10 font-['Montserrat']">
+      <div className="bg-blue-600 h-[250px] w-full flex justify-center items-center mt-10 md:mt-5 font-['Montserrat']">
         <div className="flex flex-col items-center justify-center text-center">
           <p className="text-white text-center text-sm">
-            Copyright &copy; 2023. All rights reserved.
+            Copyright &copy; 2024. All rights reserved.
           </p>
-          <span className="text-white font-bold text-[20px]">M Farhan Ramadhan</span>
+          <span className="text-white font-bold text-[25px] md:text-[30px]">M Farhan Ramadhan</span>
           <span className="text-white text-[12px]">Phyiscs Departemen, Math and Nature Faculty</span>
-          <span className="text-white text-[16px] font-black">Andalas University</span>
-          <div className="flex justify-center items-center mt-4 gap-6 text-[1.5rem]">
+          <span className="text-white text-[16px] md:text-[20px] font-black">Andalas University</span>
+          <div className="flex justify-center items-center mt-4 gap-6 md:gap-10 text-[1.5rem] md:text-[2rem]">
                 <Link to="https://www.instagram.com/farhan_r45/" className="text-gray-300 hover:text-white cursor-pointer"><FontAwesomeIcon icon={faInstagram}/></Link>
                 <Link to="/" className="text-gray-300 hover:text-white cursor-pointer"><FontAwesomeIcon icon={faFacebook}/></Link>
                 <Link to="https://github.com/Frhan21" className="text-gray-300 hover:text-white cursor-pointer"><FontAwesomeIcon icon={faGithub}/></Link>
